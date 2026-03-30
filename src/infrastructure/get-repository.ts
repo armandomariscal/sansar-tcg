e3aa3d4f9ddcf5b3908867bc0a1ae61ca5973c33
Armando Mariscal <armando.marisscal@gmail.com>
2026-03-29 21:02:14 -0600

    refactor: remove mock repository and use SQLite directly

--- src/infrastructure/get-repository.ts
+++ src/infrastructure/get-repository.ts
@@ -1,8 +1,3 @@
 import { SQLiteCardRepository } from './sqlite-repository';
-import { MockCardRepository } from './mock-repository';
 
-const PROVIDER = 'SQLITE'; 
-
-export const cardRepository = PROVIDER === 'SQLITE' 
-  ? new SQLiteCardRepository() 
-  : new MockCardRepository();
+export const cardRepository = new SQLiteCardRepository();
