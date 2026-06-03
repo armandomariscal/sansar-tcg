90925da2e939db1c983d7f870a8a13e19d492d08
Armando Mariscal <armando.marisscal@gmail.com>
2026-06-02 23:55:06 -0600

    feat(infra): introduce PostgreSQL persistence provider

--- src/infrastructure/get-repository.ts
+++ src/infrastructure/get-repository.ts
@@ -1,3 +1,7 @@
-import { SQLiteCardRepository } from './sqlite-repository';
+import { PostgresCardRepository } from "./postgres-repository";
+import { SQLiteCardRepository } from "./sqlite-repository";
 
-export const cardRepository = new SQLiteCardRepository();
+export const cardRepository =
+  process.env.DB_PROVIDER === "postgres"
+    ? new PostgresCardRepository()
+    : new SQLiteCardRepository();
